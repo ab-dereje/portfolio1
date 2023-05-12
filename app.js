853b9b8514f536f58d7b6952ab388b90f04bc3df
Abdi Dereje <abudereje09@gmail.com>
2023-05-12 19:03:01 -0400

    7th commit

--- app.js
+++ app.js
@@ -81,6 +81,11 @@ function PageTransitions(){
         }
     })
     );
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',()=>{
+        let element =document.body;
+        element.classList.toggle('light-mode');
+    })
 }
 
 PageTransitions();
