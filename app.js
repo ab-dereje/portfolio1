54275ad0aec673f0d052a0b0092e230ab5ee5408
ab-dereje <abudereje09@gmail.com>
2023-06-07 03:41:49 -0400

    9th commit

--- app.js
+++ app.js
@@ -1,91 +1,41 @@
-// const sections = document.querySelectorAll('.section');
-// const sectBtns = document.querySelectorAll('.controlls');
-// const sectBtn = document.querySelectorAll('.control');
-// const allSections = document.querySelectorAll('.main-content');
-
-// function pageTransition(){
-//     //Button click active class
-//     for(let i=0; i< sectBtn.length; i++){
-//         sectBtn[i].addEventListener('click', function(){
-//             let currentBtn = document.querySelectorAll('.active-btn');
-//             currentBtn[0].className = currentBtn[0].className.replace ('active-btn','');
-//             this.className += 'active-btn';
-//         })
-//     }
-//     //section Active
-//     allSections.addEventListener('click',(e)=>{
-//         const id= e.target.dataset.id;
-//         if (id){
-//             //remove active btn
-//             sectBtns.forEach((btn) =>{
-//                 btn.classList.remove('active')
-//             })
-//             e.target.classList.add('active')
-//             //hide other section
-//             sections.forEach((section)=>{
-//                 section.classList.remove('active')
-//             })
-//             const element = document.getElementById(id)
-//             element.classList.add("active");
-//         }
-//     })
-// }
-
-// pageTransition();
-
-// Add event listener to each button
-// sectBtn.forEach(sectBtn =>{
-//     sectBtn.addEventListener('click',()=>{
-//         //Remove active class from current active button
-//         const currentBtn =document.querySelector('.active-btn');
-//         currentBtn.classList.remove('active-btn');
-
-//         // add active class to clicked button
-//         sectBtn.classList.add('active-btn');
-//     });
-// });
-
-
-
 const sections = document.querySelectorAll('.section');
 const secBtns = document.querySelectorAll('.controlls');
 const secBtn = document.querySelectorAll('.control');
 const allSections = document.querySelectorAll('.main-content');
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('.nav-links');
 
-function PageTransitions(){
-    
-    for(let i = 0; i < secBtn.length; i++){
-        secBtn[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '')
-            this.className += ' active-btn';
-        })
-    }
+const more = document.querySelector(".more");
+const btn_text = document.querySelector(".btn-text1");
 
-    allSections.forEach((section) =>
-    section.addEventListener("click", (e) => {
-    
-    const id = e.target.dataset.id;
-    
-    if (id) {
-        secBtns.forEach((btn) => {
-        btn.classList.remove("active");
-        });
-        e.target.classList.add("active");
-        sections.forEach((section) => {
-        section.classList.remove("active");
-        });
+function PageTransitions() {
 
-        const element = document.getElementById(id);
-        element.classList.add("active");
+    // const themeBtn = document.querySelector('.theme-btn');
+    // themeBtn.addEventListener('click', () => {
+    //         let element = document.body;
+    //         element.classList.toggle('light-mode');
+    //     })
+    //menu
+    nav.style.display = "none";
+    menu.addEventListener('click', () => {
+        if (document.querySelector('.nav-links').style.display === "none") {
+            nav.style.display = "flex";
+            console.log("abu");
+        } else {
+            nav.style.display = "none";
         }
     })
-    );
-    const themeBtn = document.querySelector('.theme-btn');
-    themeBtn.addEventListener('click',()=>{
-        let element =document.body;
-        element.classList.toggle('light-mode');
+
+    // about read more
+    more.style.display = "none";
+    document.querySelector(".btn-con1").addEventListener('click', () => {
+        if (document.querySelector('.more').style.display === "none") {
+            more.style.display = "inline";
+            btn_text.innerHTML = "Read Less";
+        } else {
+            more.style.display = "none";
+            btn_text.innerHTML = "Read More";
+        }
     })
 }
-
 PageTransitions();
